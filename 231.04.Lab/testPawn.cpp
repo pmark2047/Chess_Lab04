850eb28de98fb7b6fb8f2294df421065da3cb642
BearBait16 <jacksonaden@gmail.com>
2025-10-17 23:17:26 -0600

    Added Lab05 test files

---
+++ 231.04.Lab/testPawn.cpp
@@ -0,0 +1,281 @@
+/***********************************************************************
+ * Source File:
+ *    TEST PAWN
+ * Author:
+ *    <your name here>
+ * Summary:
+ *    The unit tests for the pawn
+ ************************************************************************/
+
+#include "testPawn.h"
+#include "piecePawn.h"     
+#include "board.h"
+#include "uiDraw.h"
+#include <cassert>      
+
+
+
+ /*************************************
+  * GET MOVES Simple White
+  * White pawn in the middle of the board: b4b5
+  *
+  * +---a-b-c-d-e-f-g-h---+
+  * |                     |
+  * 8                     8
+  * 7                     7
+  * 6                     6
+  * 5     .               5
+  * 4    (p)              4
+  * 3                     3
+  * 2                     2
+  * 1                     1
+  * |                     |
+  * +---a-b-c-d-e-f-g-h---+
+  **************************************/
+void TestPawn::getMoves_simpleWhite()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+/*************************************
+ * GET MOVES Simple Black
+ * Black pawn in the middle of the board: b4b3
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6                     6
+ * 5                     5
+ * 4    (P)              4
+ * 3     .               3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_simpleBlack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET MOVES Initial Advance White
+ * White pawn initial advance: b2b3, b2b4
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6                     6
+ * 5                     5
+ * 4     .               4
+ * 3     .               3
+ * 2    (p)              2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_initialAdvanceWhite()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+/*************************************
+ * GET MOVES Initial Advance Black
+ * Black pawn initial advance: c7c6, c7c5
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7      (P)            7
+ * 6       .             6
+ * 5       .             5
+ * 4                     4
+ * 3                     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_initialAdvanceBlack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET MOVES Capture White
+ * Double capture with b7 blocked: b6a7p, b6c7p
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7   P P P             7
+ * 6    (p)              6
+ * 5                     5
+ * 4                     4
+ * 3                     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_captureWhite()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET MOVES Capture Black
+ * Double capture with b5 blocked: b6a5p, b6c5p
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6    (P)              6
+ * 5   p p p             5
+ * 4                     4
+ * 3                     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_captureBlack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+/*************************************
+ * GET MOVES Enpassant White
+ * Enpassant with b6 blocked: b5a6E, b5c6E
+ * 
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6   . P .             6
+ * 5   P(p)P             5
+ * 4                     4
+ * 3                     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_enpassantWhite()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET MOVES Enpassant Black
+ * Enpassant with f3 blocked: f4g3E, f4e3E
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6                     6
+ * 5                     5
+ * 4           p(P)p     4
+ * 3           . p .     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_enpassantBlack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET MOVES Enpassant Denied
+ * Enpassant: b6 is blocked with black pawn
+ *            pawn at a5 moved twice
+ *            pawn at c5 move two turns ago
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6     P               6
+ * 5   P(p)P             5
+ * 4                     4
+ * 3                     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_enpassantDenied()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+/*************************************
+ * GET MOVES Promotion White
+ * Promotion white pawn b7 can capture two pawns: a8 and c8. 
+ *          It can also move into b8. All three are promoted to queen: 
+ *          b7a8pQ, b7b8Q, b7c8pQ
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8   P . P             8
+ * 7    (p)              7
+ * 6                     6
+ * 5                     5
+ * 4                     4
+ * 3                     3
+ * 2                     2
+ * 1                     1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_promotionWhite()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET MOVES Promotion Black
+ * Promotion: black pawn e2 can capture two rooks: d1 and f1.
+ *            It can also move into e1. All three are promoted to quee
+ *            e2d1rQ, e2e1Q, e2f1rQ
+ *
+ * +---a-b-c-d-e-f-g-h---+
+ * |                     |
+ * 8                     8
+ * 7                     7
+ * 6                     6
+ * 5                     5
+ * 4                     4
+ * 3                     3
+ * 2          (P)        2
+ * 1         r . r       1
+ * |                     |
+ * +---a-b-c-d-e-f-g-h---+
+ **************************************/
+void TestPawn::getMoves_promotionBlack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/*************************************
+ * GET TYPE : pawn
+ * Input:
+ * Output: PAWN
+ **************************************/
+void TestPawn::getType()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
