edf0582532d616f92e7dd8ca5339d7b174b3b822
pmark2047 <peytonmarkus24@gmail.com>
2025-10-25 13:36:26 -0600

    rook and bishop at 100%

--- 231.04.Lab/testRook.cpp
+++ 231.04.Lab/testRook.cpp
@@ -30,7 +30,25 @@
  **************************************/
 void TestRook::getMoves_blocked()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   // SETUP
+   BoardEmpty board;
+   Rook rook(7, 7, false /*white*/);
+   White white;
+   rook.fWhite = true;
+   rook.position.colRow = 0x21;
+   board.board[1][1] = board.board[3][1] = &white;
+   board.board[2][0] = board.board[2][2] = &white;
+   set <Move> moves;
+   
+   // EXERCISE
+   rook.getMoves(moves, board);
+   
+   // VERIFY
+   assertUnit(moves.size() == 0); // no possible moves
+   
+   // TEARDOWN
+   board.board[1][1] = board.board[3][1] = nullptr;
+   board.board[2][0] = board.board[2][2] = nullptr;
 }
 
 
@@ -50,7 +68,54 @@ void TestRook::getMoves_blocked()
  **************************************/
 void TestRook::getMoves_slideToEnd()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   // SETUP
+   BoardEmpty board;
+   Rook rook(7, 7, false /*white*/);
+   rook.fWhite = true;
+   rook.position.colRow = 0x21;
+   set <Move> moves;
+   Move c2a2, c2b2, c2d2, c2e2, c2f2, c2g2,
+   c2h2, c2c1, c2c3, c2c4, c2c5, c2c6, c2c7, c2c8;
+   c2a2.capture = c2b2.capture = c2d2.capture = c2e2.capture = c2f2.capture =
+   c2g2.capture = c2h2.capture = c2c1.capture = c2c3.capture = c2c4.capture =
+   c2c5.capture = c2c6.capture = c2c7.capture = c2c8.capture = PieceType::SPACE;
+   c2a2.source.colRow = c2b2.source.colRow = c2d2.source.colRow = c2e2.source.colRow = c2f2.source.colRow =
+   c2g2.source.colRow = c2h2.source.colRow = c2c1.source.colRow = c2c3.source.colRow = c2c4.source.colRow =
+   c2c5.source.colRow = c2c6.source.colRow = c2c7.source.colRow = c2c8.source.colRow = 0x21;
+   c2a2.dest.colRow = 0x01;
+   c2b2.dest.colRow = 0x11;
+   c2d2.dest.colRow = 0x31;
+   c2e2.dest.colRow = 0x41;
+   c2f2.dest.colRow = 0x51;
+   c2g2.dest.colRow = 0x61;
+   c2h2.dest.colRow = 0x71;
+   c2c1.dest.colRow = 0x20;
+   c2c3.dest.colRow = 0x22;
+   c2c4.dest.colRow = 0x23;
+   c2c5.dest.colRow = 0x24;
+   c2c6.dest.colRow = 0x25;
+   c2c7.dest.colRow = 0x26;
+   c2c8.dest.colRow = 0x27;
+   
+   // EXERCISE
+   rook.getMoves(moves, board);
+   
+   // VERIFY
+   assertUnit(moves.size() == 14); // 14 possible moves
+   assertUnit(moves.find(c2a2) != moves.end());
+   assertUnit(moves.find(c2b2) != moves.end());
+   assertUnit(moves.find(c2d2) != moves.end());
+   assertUnit(moves.find(c2e2) != moves.end());
+   assertUnit(moves.find(c2f2) != moves.end());
+   assertUnit(moves.find(c2g2) != moves.end());
+   assertUnit(moves.find(c2h2) != moves.end());
+   assertUnit(moves.find(c2c1) != moves.end());
+   assertUnit(moves.find(c2c3) != moves.end());
+   assertUnit(moves.find(c2c4) != moves.end());
+   assertUnit(moves.find(c2c5) != moves.end());
+   assertUnit(moves.find(c2c6) != moves.end());
+   assertUnit(moves.find(c2c7) != moves.end());
+   assertUnit(moves.find(c2c8) != moves.end());
 }
 
 
@@ -70,7 +135,54 @@ void TestRook::getMoves_slideToEnd()
  **************************************/
 void TestRook::getMoves_slideToBlock()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   // SETUP
+   BoardEmpty board;
+   Rook rook(7, 7, false /*white*/);
+   White white;
+   rook.fWhite = true;
+   rook.position.colRow = 0x21;
+   board.board[0][1] = board.board[7][1] = &white;
+   board.board[2][0] = board.board[2][7] = &white;
+   set <Move> moves;
+   Move c2b2, c2d2, c2e2, c2f2, c2g2,
+   c2c3, c2c4, c2c5, c2c6, c2c7;
+   c2b2.capture = c2d2.capture = c2e2.capture = c2f2.capture =
+   c2g2.capture = c2c3.capture = c2c4.capture =
+   c2c5.capture = c2c6.capture = c2c7.capture = PieceType::SPACE;
+   c2b2.source.colRow = c2d2.source.colRow = c2e2.source.colRow = c2f2.source.colRow =
+   c2g2.source.colRow = c2c3.source.colRow = c2c4.source.colRow =
+   c2c5.source.colRow = c2c6.source.colRow = c2c7.source.colRow = 0x21;
+   c2b2.dest.colRow = 0x11;
+   c2d2.dest.colRow = 0x31;
+   c2e2.dest.colRow = 0x41;
+   c2f2.dest.colRow = 0x51;
+   c2g2.dest.colRow = 0x61;
+   c2c3.dest.colRow = 0x22;
+   c2c4.dest.colRow = 0x23;
+   c2c5.dest.colRow = 0x24;
+   c2c6.dest.colRow = 0x25;
+   c2c7.dest.colRow = 0x26;
+   
+   // EXERCISE
+   rook.getMoves(moves, board);
+   
+   // VERIFY
+   assertUnit(moves.size() == 10); // 14 possible moves
+   assertUnit(moves.find(c2b2) != moves.end());
+   assertUnit(moves.find(c2d2) != moves.end());
+   assertUnit(moves.find(c2e2) != moves.end());
+   assertUnit(moves.find(c2f2) != moves.end());
+   assertUnit(moves.find(c2g2) != moves.end());
+   assertUnit(moves.find(c2c3) != moves.end());
+   assertUnit(moves.find(c2c4) != moves.end());
+   assertUnit(moves.find(c2c5) != moves.end());
+   assertUnit(moves.find(c2c6) != moves.end());
+   assertUnit(moves.find(c2c7) != moves.end());
+   
+   // TEARDOWN
+   board.board[0][1] = board.board[7][1] = nullptr;
+   board.board[2][0] = board.board[2][7] = nullptr;
+   
 }
 
 /*************************************
@@ -89,7 +201,62 @@ void TestRook::getMoves_slideToBlock()
  **************************************/
 void TestRook::getMoves_slideToCapture()
 {
-   assertUnit(NOT_YET_IMPLEMENTED);
+   // SETUP
+   BoardEmpty board;
+   Rook rook(7, 7, false /*white*/);
+   Black black;
+   rook.fWhite = true;
+   rook.position.colRow = 0x21;
+   board.board[0][1] = board.board[7][1] = &black;
+   board.board[2][0] = board.board[2][7] = &black;
+   set <Move> moves;
+   Move c2a2, c2b2, c2d2, c2e2, c2f2, c2g2,
+   c2h2, c2c1, c2c3, c2c4, c2c5, c2c6, c2c7, c2c8;
+   c2a2.capture = c2b2.capture = c2d2.capture = c2e2.capture = c2f2.capture =
+   c2g2.capture = c2h2.capture = c2c1.capture = c2c3.capture = c2c4.capture =
+   c2c5.capture = c2c6.capture = c2c7.capture = c2c8.capture = PieceType::SPACE;
+   c2c1.capture = c2c8.capture = c2a2.capture = c2h2.capture = PieceType::PAWN;
+   c2a2.source.colRow = c2b2.source.colRow = c2d2.source.colRow = c2e2.source.colRow = c2f2.source.colRow =
+   c2g2.source.colRow = c2h2.source.colRow = c2c1.source.colRow = c2c3.source.colRow = c2c4.source.colRow =
+   c2c5.source.colRow = c2c6.source.colRow = c2c7.source.colRow = c2c8.source.colRow = 0x21;
+   c2a2.dest.colRow = 0x01;
+   c2b2.dest.colRow = 0x11;
+   c2d2.dest.colRow = 0x31;
+   c2e2.dest.colRow = 0x41;
+   c2f2.dest.colRow = 0x51;
+   c2g2.dest.colRow = 0x61;
+   c2h2.dest.colRow = 0x71;
+   c2c1.dest.colRow = 0x20;
+   c2c3.dest.colRow = 0x22;
+   c2c4.dest.colRow = 0x23;
+   c2c5.dest.colRow = 0x24;
+   c2c6.dest.colRow = 0x25;
+   c2c7.dest.colRow = 0x26;
+   c2c8.dest.colRow = 0x27;
+   
+   // EXERCISE
+   rook.getMoves(moves, board);
+   
+   // VERIFY
+   assertUnit(moves.size() == 14); // 14 possible moves
+   assertUnit(moves.find(c2a2) != moves.end());
+   assertUnit(moves.find(c2b2) != moves.end());
+   assertUnit(moves.find(c2d2) != moves.end());
+   assertUnit(moves.find(c2e2) != moves.end());
+   assertUnit(moves.find(c2f2) != moves.end());
+   assertUnit(moves.find(c2g2) != moves.end());
+   assertUnit(moves.find(c2h2) != moves.end());
+   assertUnit(moves.find(c2c1) != moves.end());
+   assertUnit(moves.find(c2c3) != moves.end());
+   assertUnit(moves.find(c2c4) != moves.end());
+   assertUnit(moves.find(c2c5) != moves.end());
+   assertUnit(moves.find(c2c6) != moves.end());
+   assertUnit(moves.find(c2c7) != moves.end());
+   assertUnit(moves.find(c2c8) != moves.end());
+   
+   // TEARDOWN
+   board.board[0][1] = board.board[7][1] = nullptr;
+   board.board[2][0] = board.board[2][7] = nullptr;
 }
 
 
